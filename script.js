78522d14ff66e06200dbf812878291dc4a9d86c7
Johnathan <jonathanumana86@gmail.com>
2021-10-18 19:15:01 -0400

    Added a event listener

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Number!'
+// document.querySelector('.message').textContent = '🎉 Correct Number!'
 
-document.querySelector('.number').textContent = 13
-document.querySelector('.score').textContent = 10
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 10
 
-document.querySelector('.guess').value = 25
+// document.querySelector('.guess').value = 25
+
+document.querySelector('.check').addEventListener('click', function (){
+    console.log(document.querySelector('.guess').value);
+})
