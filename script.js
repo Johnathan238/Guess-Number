720782639063dd4517da28e714337db7ea5b0c43
Johnathan238 <jonathanumana86@gmail.com>
2021-11-03 00:58:49 -0400

    Made if/else for highscore to see the new highscore

--- script.js
+++ script.js
@@ -10,8 +10,8 @@
 // document.querySelector('.guess').value = 25
 
 let number = Math.trunc(Math.random() * 20) + 1
-
 let score = 20;
+let highScore = 0
 
 document.querySelector('.check').addEventListener('click', function (){
     const guess = Number(document.querySelector('.guess').value)
@@ -28,6 +28,11 @@ document.querySelector('.check').addEventListener('click', function (){
       document.querySelector('body').style.backgroundColor = '#60b347'
 
       document.querySelector('.number').style.width = '30rem'
+
+      if (score > highScore) {
+        highScore = score
+        document.querySelector('.highscore').textContent = highScore
+      }
 
       // when guess is too high
     } else if (guess > number){
